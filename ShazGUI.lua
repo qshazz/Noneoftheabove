35c5f4577bb193828b75d2555740345eb14e2b1c
qshazz <qshazmodz@gmail.com>
2025-04-11 02:53:29 +0800

    Update ShazGUI.lua

--- ShazGUI.lua
+++ ShazGUI.lua
@@ -1,109 +1,99 @@
--- Load the Redz Hub UI Library
-local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/Library-ui/main/Redzhubui"))()
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/qshazz/Noneoftheabove/main/Source.lua"))()
 
--- Create main window
 local Window = redzlib:MakeWindow({
-    Title = "redz Hub : Blox Fruits",
-    SubTitle = "by redz9999",
-    SaveFolder = "RedzHubConfig"
+  Title = "redz Hub : Blox Fruits",
+  SubTitle = "by redz9999",
+  SaveFolder = "RedzHubConfig"
 })
 
--- Add minimize button (optional styling)
+-- Add minimize button
 Window:AddMinimizeButton({
-    Button = { Image = "rbxassetid://7101487387", BackgroundTransparency = 0 },
-    Corner = { CornerRadius = UDim.new(0, 35) },
+  Button = { Image = "rbxassetid://7101487387", BackgroundTransparency = 0 },
+  Corner = { CornerRadius = UDim.new(0, 35) }
 })
 
--- Create tab
-local Tab1 = Window:MakeTab({
-    Name = "Main Tab",
-    Icon = "cherry" -- Using text icon
-})
-
--- Set theme and select tab
+-- Set theme
 redzlib:SetTheme("Dark")
+
+-- Create tab using the correct pattern
+local Tab1 = Window:MakeTab({"Main Tab", "cherry"})
+
+-- Select tab
 Window:SelectTab(Tab1)
 
--- Add section
-local Section = Tab1:AddSection({
-    Name = "Main Features"
-})
+-- Add section using the correct pattern
+local Section = Tab1:AddSection({"Main Features"})
 
--- Add paragraph
-Tab1:AddParagraph({
-    Title = "Paragraph",
-    Content = "This is a Paragraph\nSecond Line"
-})
+-- Add paragraph using the correct pattern
+local Paragraph = Tab1:AddParagraph({"Information", "This is a Paragraph\nSecond Line"})
 
--- Discord Invite (optional)
+-- Add discord invite
 Tab1:AddDiscordInvite({
-    Name = "Name Hub",
-    Description = "Join server",
-    Logo = "rbxassetid://18751483361",
-    Invite = "YourDiscordInviteCodeHere" -- Replace with your invite code
+  Name = "Name Hub",
+  Description = "Join server",
+  Logo = "rbxassetid://18751483361",
+  Invite = "YourDiscordInviteCodeHere" -- Replace with your invite code
 })
 
--- Button
-Tab1:AddButton({
-    Name = "Print",
-    Callback = function()
-        print("Hello World!")
-    end
-})
+-- Add button using the correct pattern
+Tab1:AddButton({"Print", function()
+  print("Hello World!")
+end})
 
--- Toggle with callback
+-- Add toggle
 local Toggle1 = Tab1:AddToggle({
-    Name = "Toggle",
-    Description = "This is a Toggle Example",
-    Default = false,
-    Callback = function(Value)
-        print("Toggle value:", Value)
-    end
+  Name = "Toggle",
+  Description = "This is a Toggle Example",
+  Default = false,
+  Callback = function(Value)
+    print("Toggle value:", Value)
+  end
 })
 
--- Slider
+-- Add slider
 Tab1:AddSlider({
-    Name = "Speed",
-    Min = 1,
-    Max = 100,
-    Increase = 1,
-    Default = 16,
-    Callback = function(Value)
-        print("Speed:", Value)
-    end
+  Name = "Speed",
+  Min = 1,
+  Max = 100,
+  Increase = 1,
+  Default = 16,
+  Callback = function(Value)
+    print("Speed:", Value)
+    -- Uncomment if you want to change player's speed
+    -- game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+  end
 })
 
--- Dropdown
+-- Add dropdown
 Tab1:AddDropdown({
-    Name = "Players List",
-    Description = "Select the Number",
-    Options = {"one", "two", "three"},
-    Default = "two",
-    Callback = function(Value)
-        print("Dropdown selected:", Value)
-    end
+  Name = "Players List",
+  Description = "Select the Number",
+  Options = {"one", "two", "three"},
+  Default = "two",
+  Callback = function(Value)
+    print("Dropdown selected:", Value)
+  end
 })
 
--- Textbox
+-- Add textbox
 Tab1:AddTextBox({
-    Name = "Name item",
-    Description = "1 Item on 1 Server",
-    PlaceholderText = "item only",
-    Callback = function(Value)
-        print("Text input:", Value)
-    end
+  Name = "Name item",
+  Description = "1 Item on 1 Server",
+  PlaceholderText = "item only",
+  Callback = function(Value)
+    print("Text input:", Value)
+  end
 })
 
--- Dialog popup (moved after all elements to prevent early triggering)
--- Uncomment if you want to show a dialog
+-- Dialog (uncomment if you want to use it)
 --[[
-Window:Dialog({
-    Title = "Dialog",
-    Text = "This is a Dialog",
-    Options = {
-        {"Confirm", function() print("Confirmed!") end},
-        {"Maybe", function() print("Maybe...") end},
-        {"Cancel", function() print("Canceled") end}
-    }
+local Dialog = Window:Dialog({
+  Title = "Dialog",
+  Text = "This is a Dialog",
+  Options = {
+    {"Confirm", function() print("Confirmed!") end},
+    {"Maybe", function() print("Maybe...") end},
+    {"Cancel", function() print("Canceled") end}
+  }
 })
 ]]
